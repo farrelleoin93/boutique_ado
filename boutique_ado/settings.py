981cd186a7bde4a87d22f5d4c50ac2d6440204a1
farrelleoin93 <farrelleoin93@gmail.com>
2021-07-06 22:42:32 +0000

    deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-swf+3pp!liyo$u(kzd=^07s$(2+4&6br@x-y=jnj_bm7v-41yx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farrelleoin93-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +124,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
