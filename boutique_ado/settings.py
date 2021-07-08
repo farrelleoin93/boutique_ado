5d8e9f75af0b146a25637ea2faeb557c7312be95
farrelleoin93 <farrelleoin93@gmail.com>
2021-07-08 17:56:45 +0000

    Set up to sen real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -211,7 +211,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-f 'DEVELOPMENT' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
